489e618fae55427bbdaeb9558cbdd1f370c67862
Franco Chen <francochen2000@hotmail.com>
2020-08-11 17:25:47 -0400

    Begins work on navbar

---
+++ client/src/component/MenuItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "@chakra-ui/core/dist/Heading";
+
+class MenuItem extends React.Component {
+
+    render() {
+        return (
+            <Heading size="sm" marginRight={this.props.lastItem ? "0px" : "65px"}>
+                {this.props.children}
+            </Heading>
+        );
+    }
+}
+
+export default MenuItem;
