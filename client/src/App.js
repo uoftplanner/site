06a5440206115b7ce2c92fe490a91702668e0a64
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2020-08-14 20:17:21 -0400

    Setup react-router-dom for routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
 import {CSSReset, ThemeProvider} from '@chakra-ui/core'
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from "react-router-dom";
 import './App.css';
 import Header from './component/Header'
+import HomePage from './views/HomePage'
 
 class App extends React.Component {
-    render() {
-        return (
-            <ThemeProvider>
-                <CSSReset/>
-                <Header/>
-            </ThemeProvider>
-        );
-    }
+	render() {
+		return (
+			<ThemeProvider>
+				<CSSReset />
+				<Header />
+				<Router>
+					<Switch>
+						<Route exact path='/' component={HomePage} />
+					</Switch>
+				</Router>
+			</ThemeProvider>
+		);
+	}
 }
 
 export default App;
