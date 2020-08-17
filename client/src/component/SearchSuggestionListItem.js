26712960e900e5a55614a480db50150ed974cfc9
Franco Chen <francochen2000@hotmail.com>
2020-08-17 19:09:21 -0400

    Add units to search suggestion padding

--- client/src/component/SearchSuggestionListItem.js
+++ client/src/component/SearchSuggestionListItem.js
@@ -17,8 +17,8 @@ class SearchSuggestionListItem extends React.Component {
                 as={Link}
                 to={"/course/" + code}
                 display="flex"
-                paddingTop='3'
-                paddingBottom='3'
+                paddingTop='3px'
+                paddingBottom='3px'
                 key={code}
                 alignItems="center">
 
