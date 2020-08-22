6b802701027f602d0a1580233c913491a90e0984
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2020-08-21 20:42:17 -0400

    Create route and links for login page

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,18 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Header from './component/Header';
 import HomePage from './views/HomePage';
+import LoginPage from './views/LoginPage';
 
 class App extends React.PureComponent {
   render() {
     return (
       <ChakraProvider theme={theme}>
         <CSSReset />
-        <Header />
         <Router>
+          <Header />
           <Switch>
             <Route exact path="/" component={HomePage} />
+            <Route exact path="/login" component={LoginPage} />
           </Switch>
         </Router>
       </ChakraProvider>
--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import {Button, Flex, Heading} from '@chakra-ui/core';
+import {Link} from 'react-router-dom';
 import MenuItem from './MenuItem';
 
-class Header extends React.PureComponent {
-  render() {
-    const headerStyle = {
-      padding: '34px 61px',
-    };
+const headerStyle = {
+  padding: '34px 61px',
+};
 
-    return (
+function Header() {
+  return (
       <Flex justifyContent="space-between" alignItems="center" wrap="wrap" style={headerStyle}>
-        <Heading as="h1" size="xl">
-          site name
-        </Heading>
+        <Link to="/">
+          <Heading as="h1" size="xl">
+            site name
+          </Heading>
+        </Link>
 
         <Flex display={{"base": "none", "md": "flex"}}
           alignItems="center"
@@ -23,14 +25,15 @@ class Header extends React.PureComponent {
         </Flex>
 
         <Flex alignItems="center">
-          <Button marginRight={{"base": "0px", "md": "38px"}} variant="outline">
-            login
-          </Button>
+          <Link to="/login">
+            <Button marginRight={{"base": "0px", "md": "38px"}} variant="outline">
+              login
+      </Button>
+          </Link>
           <Button display={{"base": "none", "md": "flex"}}>sign up</Button>
         </Flex>
       </Flex>
-    );
-  }
+  );
 }
 
 export default Header;
