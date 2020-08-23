db097c6d93f90a1bf928a66bb6961bbd5bb5205d
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2020-08-22 20:53:55 -0400

    Add blank keys

--- server/config/dev_keys.js
+++ server/config/dev_keys.js
@@ -1,3 +1,15 @@
 module.exports = {
   redisHost: 'YOUR REDIS DEVELOPMENT HOST HERE',
+  mongoUri: 'MONGOURI',
+  cookieKey: 'SECRET KEY FOR COOKIE-SESSION',
+  oauth: {
+    google: {
+      clientId: 'GOOGLE APP ID',
+      clientSecret: 'GOOGLE APP SECRET'
+    },
+    facebook: {
+      clientId: 'FACEBOOK APP ID',
+      clientSecret: 'FACEBOOK APP SECRET'
+    }
+  }
 };
